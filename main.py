ab92fe3f6e80c8327bcded9dd45cc7ef17ada83d
ryanhan1201 <ryanhan1201@gmail.com>
2022-07-20 15:30:30 -0700

    Added location checker win con checker and tie checker

--- main.py
+++ main.py
@@ -17,11 +17,30 @@ def main():
                 if board[i][j] == ' ':
                     return False
         return True
-    #def check_win():
+    def check_win(turn):
+        #there are 8 three in a rows so make vars for each win con
+        con1 = board[0][0] == board[0][1] and board[0][0] == board[0][2]
+        con2 = board[1][0] == board[1][1] and board[1][0] == board[1][2]
+        con3 = board[2][0] == board[2][1] and board[2][0] == board[2][2]
+        con4 = board[0][0] == board[1][0] and board[0][0] == board[2][0]
+        con5 = board[0][1] == board[1][1] and board[0][1] == board[2][1]
+        con6 = board[0][2] == board[1][2] and board[0][2] == board[2][2]
+        con7 = board[0][0] == board[1][1] and board[0][0] == board[2][2]
+        con8 = board[0][2] == board[1][1] and board[0][2] == board[2][0]
+
+        if turn % 2 == 0:
+            #checking for O win
+            return (con1 and board[0][0] == 'O') or (con2 and board[1][0] == 'O') or (con3 and board[2][0] == 'O') or (con4 and board[0][0] == 'O') or (con5 and board[0][1] == 'O') or (con6 and board[0][2] == 'O') or (con7 and board[0][0] == 'O') or (con8 and board[0][2] == 'O')
+        else:
+            #checking for X win
+            return (con1 and board[0][0] == 'X') or (con2 and board[1][0] == 'X') or (con3 and board[2][0] == 'X') or (con4 and board[0][0] == 'X') or (con5 and board[0][1] == 'X') or (con6 and board[0][2] == 'X') or (con7 and board[0][0] == 'X') or (con8 and board[0][2] == 'X')
+
     def check_loc(i, j):
         return board[i][j] != ' '
+    print_board(board)
     turn_counter = 0
-    while not is_filled():
+    win_con = False
+    while not is_filled() and not win_con:
         #if turn counter is even then its P1's turn
         #if turn counter is odd then its P2's turn
         loc = 0
@@ -30,8 +49,13 @@ def main():
             loc = int(input())
             x = int(loc / 3)
             y = loc % 3
-            if check_loc(x, y):
-                print("That location is filled. Try again")
+            if loc > 9:
+                print("That is not a playable space.")
+                print("Try Again")
+                turn_counter -= 1
+            elif check_loc(x, y):
+                print("That location is filled.")
+                print("Try again")
                 turn_counter -= 1
             else:
                 board[x][y] = 'O'
@@ -39,14 +63,26 @@ def main():
             loc = int(input())
             x = int(loc / 3)
             y = loc % 3
-            if check_loc(x, y):
+            if loc >= 9:
+                print("That is not a playable space.")
+                print("Try Again")
+                turn_counter -= 1
+            elif check_loc(x, y):
                 print("That location is filled. Try again")
                 turn_counter -= 1
             else:
                 board[x][y] = 'X'
-        turn_counter += 1
         print_board(board)
-
+        win_con = check_win(turn_counter)
+        turn_counter += 1
+    if win_con:
+        winner = 0
+        if turn_counter % 2 == 0:
+            winner = 2
+        else:
+            winner = 1
+        print("The Winner Is Player", winner)
+    else:
+        print("Its A TIE")
 
 main()
-#TODO: Implement win checker, also error check if input of location is too high 
